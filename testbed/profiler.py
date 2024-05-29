1b104c7acb3e3ba2fa3e325281a409b04429f852
nephrencake <834957063@qq.com>
2024-05-29 14:31:42 +0800

    profile 16 gpu throughput

--- testbed/profiler.py
+++ testbed/profiler.py
@@ -1,5 +1,10 @@
 import os
+import time
 
+import zerorpc
+
+from models.env import get_checkpoint_path
+from models.tool import get_cmd
 from policy.applications import APPLICATIONS
 
 
@@ -14,9 +19,9 @@ def get_best_placement(num_replicas):
 
 
 local_bsz_dict = {
-    "bert": [2, 4, 6, 8, 12],
+    "bert": [2, 3, 4, 6, 8, 12],
     "cifar10": [32, 64, 128, 256, 512, 1024],
-    "ncf": [1024, 2048, 4096, 8192, 16394, 32768],
+    "ncf": [256, 512, 1024, 2048, 4096, 8192, 16394, 32768],
     "imagenet": [16, 25, 32, 50, 64, 100],
     "deepspeech2": [5, 10, 20, 40, 80],
     "yolov3": [2, 4, 8, 16],
@@ -38,7 +43,7 @@ def get_all_configs(application):
     max_global_bsz = application.max_batch_size
     return [
         {
-            "job_name": f"{application.name}-{''.join(map(str, placement))}-{local_bsz}",
+            "job_name": f"profile-{application.name}-{''.join(map(str, placement))}-{local_bsz}",
             "app_name": application.name,
             "placement": placement,
             "local_bsz": local_bsz,
@@ -49,38 +54,66 @@ def get_all_configs(application):
     ]
 
 
-def get_num_node(placement):
-    return sum(map(lambda x: x > 0, placement))
-
-
-def get_unprofiled_placements():
-    pass
-
-
 if __name__ == '__main__':
-    available_node = (None, None, None, "10.0.0.23")
+    # nohup python3 -u profiler.py > ./profiler.log 2>&1 &
+    available_node = ("10.0.0.22", "10.0.0.23", "10.0.0.24", "10.0.0.26")
+    connects = {}
+    for node in available_node:
+        if node is not None:
+            connects[node] = zerorpc.Client()
+            connects[node].connect(f"tcp://{node}:4242")
     all_configs = []
     for app_name, app in APPLICATIONS.items():
-
         configs = get_all_configs(app)
         all_configs += configs
 
-        print(f"app_name = {app_name}, num_configs = {len(configs)}, all_configs = {configs}")
+        print(
+            f"app_name = {app_name}, num_configs = {len(configs)}"
+            # f", all_configs = {configs}"
+        )
 
     # 过滤已测量的
-    profiled_name = os.listdir(ckp_path)
-    all_configs = [config for config in all_configs if config["job_name"] not in profiled_name]
+    profiled_job_name = os.listdir(ckp_path)
+    all_configs = [config for config in all_configs if config["job_name"] not in profiled_job_name]
     # 过滤现在可用机器无法测量的
-    all_configs = [config for config in all_configs
-                   if get_num_node(config["placement"]) <= sum(map(lambda x: x is not None, available_node))]
+    all_configs = [config for config in all_configs if
+                   sum(map(lambda x: x > 0, config["placement"])) <= sum(map(lambda x: x is not None, available_node))]
+    print(
+        f"num_configs = {len(all_configs)}, all_configs = {all_configs}"
+    )
     # 构造命令
     for config in all_configs:
         job_name = config["job_name"]
         app_name = config["app_name"]
         placement = config["placement"]
         local_bsz = config["local_bsz"]
-        allocation =
-
-    # 循环执行并检测是否完成
-
-    # 聚合profile
+        allocation_ng = [(available_node[i], j)
+                         for i in range(4) if available_node[i] is not None
+                         for j in range(placement[i])]
+        allocation_n = [available_node[i]
+                        for i in range(4) if available_node[i] is not None
+                        for j in range(placement[i])]
+        print(
+            f"job_name = {job_name}, allocation = {allocation_ng}"
+        )
+        for rank, (node_ip, gpu_id) in enumerate(allocation_ng):
+            proc_name = f"{job_name}:{rank}"
+            cmd = get_cmd(job_name=job_name, allocation=allocation_n, rank=rank,
+                          acc_bsz=local_bsz, acc_step=1, model_name=app_name) + " --profile"
+            out_file = f"{get_checkpoint_path(job_name, return_dir=True)}/rank_{rank}.log"
+            print(f"connects[{node_ip}].run_proc({proc_name}, {cmd}, {gpu_id}, {out_file})")
+            # input()
+            connects[node_ip].run_proc(proc_name, cmd, gpu_id, out_file)
+        # input()
+        for rank, (node_ip, gpu_id) in enumerate(allocation_ng):
+            proc_name = f"{job_name}:{rank}"
+            time_out, start_time = 120, time.time()
+            while True:
+                res = connects[node_ip].stats_proc(proc_name)
+                print(f"connects[{node_ip}].stats_proc({proc_name})")
+                if res.get("returncode", None) is not None:
+                    print(f"Process {proc_name}({node_ip}:{gpu_id}) terminated with returncode {res['returncode']}")
+                    break
+                if time.time() - start_time > time_out:
+                    raise TimeoutError(f"Process {proc_name}({node_ip}:{gpu_id}) terminated due to timeout")
+                time.sleep(1)
