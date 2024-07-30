e9bc5e15837d33550dbee608384e7ec35d2b725f
nephrencake <834957063@qq.com>
2024-07-30 12:49:22 +0800

    exo-elasticity

--- testbed/models/tool.py
+++ testbed/models/tool.py
@@ -16,7 +16,7 @@ from models.env import get_python3_path
 from policy.applications import memoize, APPLICATIONS
 
 
-def set_random_seed(seed=6666):
+def set_random_seed(seed=0):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
